02dbe2e072813eb10d463dd1359883f875d4866f
solfer <fvern.witherial01@tutanota.com>
2022-02-17 18:57:44 +0000

    Added additional python flask files

--- website/auth.py
+++ website/auth.py
@@ -4,3 +4,44 @@ from . import db
 from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
+
+
+"""
+picture_link = NULL
+firstname = 'Cassis'
+surname = 'Texic'
+
+title = 'Lorem ipsum dolor sit'
+description = '''Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+Adipiscing enim eu turpis egestas pretium aenean pharetra.'''
+
+text = '''Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+Adipiscing enim eu turpis egestas pretium aenean pharetra. Donec et 
+odio pellentesque diam volutpat commodo sed. Aliquet sagittis id consectetur 
+purus ut faucibus pulvinar. Commodo sed egestas egestas fringilla phasellus 
+faucibus scelerisque eleifend donec. Metus vulputate eu scelerisque felis 
+imperdiet proin fermentum leo. Eros donec ac odio tempor orci dapibus 
+ultrices in. Nulla facilisi morbi tempus iaculis urna id. Semper quis 
+lectus nulla at volutpat. Vestibulum lectus mauris ultrices eros in cursus 
+turpis massa. Platea dictumst quisque sagittis purus sit amet volutpat 
+consequat mauris. Quisque id diam vel quam. Quisque non tellus orci ac auctor 
+augue mauris augue neque. Rhoncus aenean vel elit scelerisque mauris pellentesque. 
+Adipiscing elit pellentesque habitant morbi tristique senectus.\n
+Egestas maecenas pharetra convallis posuere morbi leo urna molestie. 
+Tellus rutrum tellus pellentesque eu. Purus sit amet luctus venenatis lectus 
+magna. Id eu nisl nunc mi. Velit laoreet id donec ultrices tincidunt arcu non 
+sodales. Fringilla urna porttitor rhoncus dolor purus non enim praesent 
+elementum. Sed vulputate mi sit amet mauris. Porta lorem mollis aliquam ut 
+porttitor leo a. Porttitor rhoncus dolor purus non enim praesent elementum. 
+Vitae tempus quam pellentesque nec nam aliquam. Et tortor consequat id porta. 
+Augue lacus viverra vitae congue eu consequat. Faucibus pulvinar elementum 
+integer enim. Consectetur adipiscing elit duis tristique sollicitudin. 
+Consectetur lorem donec massa sapien faucibus et molestie. Eget lorem dolor 
+sed viverra.'''
+
+youtube_embed_link = NULL
+writer_id = 1
+
+"""
--- website/views.py
+++ website/views.py
@@ -5,6 +5,135 @@ from json import loads
 
 views = Blueprint('views', __name__)
 
-@views.route('/', methods=['GET'])
+@views.route('/')
 def home():
-    return render_template('home.html')
+    page_settings = {
+        'cover_picture': True,
+        'current_page': 1,
+        'sidebar_segments':1 #maximum 2
+        }
+    sidebar_links = [
+        '#',
+        '#'
+    ]
+    return render_template(
+        'home.html', 
+        cover_picture=page_settings['cover_picture'], 
+        current_page_num=page_settings['current_page'], 
+        sidebar_links=sidebar_links, 
+        sidebar_segments=page_settings['sidebar_segments']
+        )
+
+@views.route('/articles')
+def articles():
+    page_settings = {
+        'cover_picture': True,
+        'current_page': 2,
+        'sidebar_segments':1 #maximum 2
+        }
+    sidebar_links = [
+        '#',
+        '#'
+    ]
+    return render_template(
+        'articles.html', 
+        cover_picture=page_settings['cover_picture'], 
+        current_page_num=page_settings['current_page'], 
+        sidebar_links=sidebar_links, 
+        sidebar_segments=page_settings['sidebar_segments']
+        )
+
+@views.route('/events')
+def events():
+    page_settings = {
+        'cover_picture': True,
+        'current_page': 3,
+        'sidebar_segments':2 #maximum 2
+        }
+    sidebar_links = [
+        '#',
+        '#'
+    ]
+    return render_template(
+        'events.html', 
+        cover_picture=page_settings['cover_picture'], 
+        current_page_num=page_settings['current_page'], 
+        sidebar_links=sidebar_links, 
+        sidebar_segments=page_settings['sidebar_segments']
+        )
+
+@views.route('/volunteer')
+def volunteer():
+    page_settings = {
+        'cover_picture': True,
+        'current_page': 4,
+        'sidebar_segments':2 #maximum 2
+        }
+    sidebar_links = [
+        '#',
+        '#'
+    ]
+    return render_template(
+        'volunteer.html', 
+        cover_picture=page_settings['cover_picture'], 
+        current_page_num=page_settings['current_page'], 
+        sidebar_links=sidebar_links, 
+        sidebar_segments=page_settings['sidebar_segments']
+    )
+
+@views.route('/support')
+def support():
+    page_settings = {
+        'cover_picture': True,
+        'current_page': 5,
+        'sidebar_segments':1 #maximum 2
+        }
+    sidebar_links = [
+        '#',
+        '#'
+    ]
+    return render_template(
+        'support.html', 
+        cover_picture=page_settings['cover_picture'], 
+        current_page_num=page_settings['current_page'], 
+        sidebar_links=sidebar_links, 
+        sidebar_segments=page_settings['sidebar_segments']
+        )
+
+@views.route('/aboutus')
+def aboutus():
+    page_settings = {
+        'cover_picture': True,
+        'current_page': 6,
+        'sidebar_segments':1 #maximum 2
+        }
+    sidebar_links = [
+        '/',
+        '#'
+    ]
+    return render_template(
+        'aboutus.html', 
+        cover_picture=page_settings['cover_picture'], 
+        current_page_num=page_settings['current_page'], 
+        sidebar_links=sidebar_links, 
+        sidebar_segments=page_settings['sidebar_segments']
+        )
+
+@views.route('/article-maker') #should be hidden
+def articleMaker():
+    page_settings = {
+        'cover_picture': False,
+        'current_page': 7,
+        'sidebar_segments':1 #maximum 2
+        }
+    sidebar_links = [
+        '/',
+        '#'
+    ]
+    return render_template(
+        'article-maker.html', 
+        cover_picture=page_settings['cover_picture'], 
+        current_page_num=page_settings['current_page'], 
+        sidebar_links=sidebar_links, 
+        sidebar_segments=page_settings['sidebar_segments']
+        )
