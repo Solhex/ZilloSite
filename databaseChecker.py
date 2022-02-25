07ff36ad7defbf1dc869f8775597527406efde49
solfer <fvern.witherial01@tutanota.com>
2022-02-25 08:52:50 +0000

    Updated / Added tempfiles

--- databaseChecker.py
+++ databaseChecker.py
@@ -1,7 +1,12 @@
 import sqlite3
 
-conn = sqlite3.connect(r'website\Databases\article_database.db')
+conn = sqlite3.connect(r'website\databases\article_database.db')
 c = conn.cursor()
 
 print(c.execute('SELECT * FROM Writer'),c.fetchall())
 print(c.execute('SELECT * FROM Article'),c.fetchall())
+
+conn = sqlite3.connect(r'website\databases\email_subscriptions.db')
+c = conn.cursor()
+
+print(c.execute('SELECT * FROM Subscription'),c.fetchall())
