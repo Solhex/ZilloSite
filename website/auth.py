13a551391a90ebf1f3c98cbdedeeb79089b8d67a
solfer <fvern.witherial01@tutanota.com>
2022-02-25 09:00:38 +0000

    removed / renamed

--- website/auth.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for
-from .models import Article, Writer
-from . import db
-from flask_login import login_user, login_required, logout_user, current_user
-
-auth = Blueprint('auth', __name__)
