ca76b81378b0b70e66252fe816b759f0dbe7b032
solfer <fvern.witherial01@tutanota.com>
2022-03-02 20:42:23 +0000

    Updated view to conform to database additions

--- website/views.py
+++ website/views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, flash, redirect, current_app
-from .models import Article, Writer, Subscription
+from .models import Article, Writer, Subscription, Event
 from sqlalchemy import desc
 from . import db
 from datetime import datetime
@@ -9,7 +9,8 @@ from PIL import Image
 
 views = Blueprint('views', __name__)
 
-EMAIL_REGEX = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+EMAIL_REGEX = r'\b[\w\d.%+-]+@[A-Za-z\d]+(\.[A-Za-z]{2,}){1,2}\b'
+POSTCODE_REGEX = r'\b[A-Za-z]{1,2}(\d{1,2}|\d[A-Za-z]) ?\d[A-Za-z]{2}\b'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'webp'}
 
 def allowed_file(filename):
@@ -86,8 +87,8 @@ def home():
         articles=dbarticles
         )
 
-@views.route('/articles', methods=['GET', 'POST'])
-def articles():
+@views.route('/articles/<int:pg>', methods=['GET', 'POST'])
+def articles(pg):
     page_settings = {
         'cover_picture': True,
         'current_page': 2,
@@ -101,17 +102,24 @@ def articles():
 
     newsletterForm()
 
+    dbarticles = Article.query.order_by(desc(Article.id)).limit(50).all()
+    
+    dbarticledivision = (-(-len(dbarticles)//5))
+
     return render_template(
         './articles.html', 
         cover_picture=page_settings['cover_picture'], 
         current_page_num=page_settings['current_page'], 
         footer=page_settings['footer'],
         sidebar_links=sidebar_links, 
-        sidebar_segments=page_settings['sidebar_segments']
+        sidebar_segments=page_settings['sidebar_segments'],
+        articles=dbarticles,
+        max_pages=dbarticledivision,
+        article_page=pg
         )
 
-@views.route('/events/events-gridview', methods=['GET', 'POST'])
-def eventsGridview():
+@views.route('/events/events-gridview/<int:pg>', methods=['GET', 'POST'])
+def eventsGridview(pg):
     page_settings = {
         'cover_picture': True,
         'current_page': 3,
@@ -119,19 +127,26 @@ def eventsGridview():
         'sidebar_segments':2 #maximum 2
         }
     sidebar_links = [
-        '#',
-        '/events/events-mapview'
+        '/events/events-mapview',
+        '#'
     ]
 
     newsletterForm()
-
+    
+    dbevents = Event.query.order_by(desc(Event.id)).limit(50).all()
+    
+    dbeventdivision = (-(-len(dbevents)//5))
+    
     return render_template(
         './events/events-gridview.html', 
         cover_picture=page_settings['cover_picture'], 
         current_page_num=page_settings['current_page'], 
         footer=page_settings['footer'],
         sidebar_links=sidebar_links, 
-        sidebar_segments=page_settings['sidebar_segments']
+        sidebar_segments=page_settings['sidebar_segments'],
+        events=dbevents,
+        max_pages=dbeventdivision,
+        event_page=pg
         )
 
 @views.route('/events/events-mapview')
@@ -143,8 +158,8 @@ def eventsMapview():
         'sidebar_segments':2 #maximum 2
         }
     sidebar_links = [
-        '/events/events-gridview',
-        '#'
+        '#',
+        '/events/events-gridview'
     ]
 
     return render_template(
@@ -156,8 +171,8 @@ def eventsMapview():
         sidebar_segments=page_settings['sidebar_segments']
         )
 
-@views.route('/volunteer', methods=['GET', 'POST'])
-def volunteer():
+@views.route('/volunteer/volunteer-gridview', methods=['GET', 'POST'])
+def volunteerGridview():
     page_settings = {
         'cover_picture': True,
         'current_page': 4,
@@ -165,14 +180,38 @@ def volunteer():
         'sidebar_segments':2 #maximum 2
         }
     sidebar_links = [
-        '#',
+        '/volunteer/volunteer-mapview',
         '#'
     ]
 
     newsletterForm()
 
     return render_template(
-        './volunteer.html', 
+        './volunteer/volunteer-gridview.html', 
+        cover_picture=page_settings['cover_picture'], 
+        current_page_num=page_settings['current_page'], 
+        footer=page_settings['footer'],
+        sidebar_links=sidebar_links, 
+        sidebar_segments=page_settings['sidebar_segments']
+    )
+    
+@views.route('/volunteer/volunteer-mapview', methods=['GET', 'POST'])
+def volunteerMapview():
+    page_settings = {
+        'cover_picture': False,
+        'current_page': 4,
+        'footer' : False,
+        'sidebar_segments':2 #maximum 2
+        }
+    sidebar_links = [
+        '#',
+        '/volunteer/volunteer-gridview'
+    ]
+
+    newsletterForm()
+
+    return render_template(
+        './volunteer/volunteer-mapview.html', 
         cover_picture=page_settings['cover_picture'], 
         current_page_num=page_settings['current_page'], 
         footer=page_settings['footer'],
@@ -180,7 +219,7 @@ def volunteer():
         sidebar_segments=page_settings['sidebar_segments']
     )
 
-@views.route('/support', methods=['GET', 'POST'])
+@views.route('/support/support-home', methods=['GET', 'POST'])
 def support():
     page_settings = {
         'cover_picture': True,
@@ -196,7 +235,7 @@ def support():
     newsletterForm()
 
     return render_template(
-        './support.html', 
+        './support/support-home.html', 
         cover_picture=page_settings['cover_picture'], 
         current_page_num=page_settings['current_page'], 
         footer=page_settings['footer'],
@@ -228,7 +267,7 @@ def aboutus():
         sidebar_segments=page_settings['sidebar_segments']
         )
 
-@views.route('hidden/article-manager', methods=['GET', 'POST']) #should be hidden
+@views.route('hidden/db-manager', methods=['GET', 'POST']) #should be hidden
 def articleManager():
     page_settings = {
         'cover_picture': False,
@@ -296,27 +335,27 @@ def articleManager():
                 return redirect(request.url)
 
             writerid = request.form.get('writerid')
-            date = request.form.get('date')
+            datetime_created = request.form.get('date')
             title = request.form.get('title')
             description = request.form.get('description')
             content = request.form.get('content')
             ytembed = request.form.get('ytembed')
 
-            dsplit = date.split('-')
-            if date != '':
+            dsplit = datetime_created.split('-')
+            if datetime_created != '':
                 dtransformed = datetime(int(dsplit[0]),int(dsplit[1]),int(dsplit[2]))
             writer_check = Writer.query.filter_by(id=writerid).first()
             if not writer_check:
                 flash('Writer does not exist.', category='error')
                 return redirect(request.url)
-            elif len(title) <= 7:
-                flash('Enter a title with more then 7 characters.', category='error')
+            elif len(title) < 7:
+                flash('Enter a title with 7 or more characters.', category='error')
                 return redirect(request.url)
             elif len(content) <= 50:
                 flash('Content must be more then 50 characters long.', category='error')
                 return redirect(request.url)
             else:
-                if date == '':
+                if datetime_created == '':
                     new_article = Article(writer_id=writerid, title=title, description=description, content=content, youtube_embed_link=ytembed)
                     db.session.add(new_article)
                     db.session.commit()
@@ -335,11 +374,90 @@ def articleManager():
                     file.save(path.join(current_app.config['UPLOAD_FOLDER'], 'article_covers', filename + '.webp'), format="webp")
                     flash('Article added.', category='success')
                     return redirect(request.url)
+                
+        elif form_name == 'add-event':
+            if 'fileupload' not in request.files:
+                flash('No file part')
+                return redirect(request.url)
+            file = request.files['fileupload']
+            if file.filename == '':
+                flash('No selected file')
+                return redirect(request.url)
+            if file and allowed_file(file.filename):
+                try:
+                    filename = str(Event.query.order_by(desc(Event.id)).first().id + 1)
+                except:
+                    filename = str(1)
+            else:
+                flash('Invalid file')
+                return redirect(request.url)
+
+            name = request.form.get('name')
+            description = request.form.get('description')
+            addr1 = request.form.get('addr1')
+            addr2 = request.form.get('addr2')
+            city = request.form.get('city')
+            county = request.form.get('county')
+            country = request.form.get('country')
+            postcode = request.form.get('postcode').upper()
+            datestart = request.form.get('datestart')
+            timestart = request.form.get('timestart')
+            dateend = request.form.get('dateend')
+            timeend = request.form.get('timeend')
+            
+            if datestart and timestart != '':
+                dsplit_start = datestart.split('-')
+                tsplit_start = timestart.split(':')
+                dstransformed = datetime(int(dsplit_start[0]),int(dsplit_start[1]),int(dsplit_start[2]),int(tsplit_start[0]),int(tsplit_start[1]))
+            else:
+                flash('Enter a start date and time.', category='error')
+                return redirect(request.url)
+            if dateend and timeend != '':
+                dsplit_end = dateend.split('-')
+                tsplit_end = timeend.split(':')
+                detransformed = datetime(int(dsplit_end[0]),int(dsplit_end[1]),int(dsplit_end[2]),int(tsplit_end[0]),int(tsplit_end[1]))
+            else:
+                flash('Enter a end date and time.', category='error')
+                return redirect(request.url)
+            
+            if dstransformed > detransformed:
+                flash('Start date cannot be greater then end date.', category='error')
+                return redirect(request.url)
+            
+            if len(name) <= 5:
+                flash('Enter a title with more then 5 characters.', category='error')
+                return redirect(request.url)
+            elif len(description) <= 50:
+                flash('Content must be more then 50 characters long.', category='error')
+                return redirect(request.url)
+            elif len(addr1) < 5:
+                flash('Address 1 must be 5 or more characters long.', category='error')
+                return redirect(request.url)
+            elif len(city) < 3:
+                flash('City must be 3 or more characters long.', category='error')
+                return redirect(request.url)
+            elif len(country) < 5:
+                flash('Country must be 5 or more characters long.', category='error')
+                return redirect(request.url)
+            elif not fullmatch(POSTCODE_REGEX, postcode):
+                flash('Enter a valid postcode address.', category='error')
+                return redirect(request.url)
+            elif detransformed < datetime.now():
+                flash('End date cannot be in the past.', category='error')
+                return redirect(request.url)
+            else:
+                new_event = Event(name=name, description=description, address1=addr1, address2=addr2, city=city, county=county, country=country, postcode=postcode, datetime_end=detransformed, datetime_start=dstransformed)
+                db.session.add(new_event)
+                db.session.commit()
+                file = Image.open(file)
+                file.save(path.join(current_app.config['UPLOAD_FOLDER'], 'event_covers', filename + '.webp'), format="webp")
+                flash('Event added.', category='success')
+                return redirect(request.url)
 
         elif form_name == 'remove-subscriber':
             subscriberid = request.form.get('subscriberid')
             subscriber_check = Subscription.query.filter_by(id=subscriberid).first()
-            if not writer_check:
+            if not subscriber_check:
                 flash('Subscriber does not exist.', category='error')
                 return redirect(request.url)
             else:
@@ -380,6 +498,20 @@ def articleManager():
                     remove(path.join(current_app.config['UPLOAD_FOLDER'], 'article_covers', str(article_check.id) + '.webp'))
                 flash('Article has been deleted.')
                 return redirect(request.url)
+            
+        elif form_name == 'remove-event':
+            eventid = request.form.get('eventid')
+            event_check = Event.query.filter_by(id=eventid).first()
+            if not event_check:
+                flash('Event does not exist.', category='error')
+                return redirect(request.url)
+            else:
+                db.session.delete(event_check)
+                db.session.commit()
+                if path.exists(path.join(current_app.config['UPLOAD_FOLDER'], 'event_covers', str(event_check.id) + '.webp')):
+                    remove(path.join(current_app.config['UPLOAD_FOLDER'], 'event_covers', str(event_check.id) + '.webp'))
+                flash('Event has been deleted.')
+                return redirect(request.url)
 
         elif form_name == 'email_subscription':
             email = request.form.get('newsletter')
@@ -399,7 +531,7 @@ def articleManager():
                     return redirect(request.url)
 
     return render_template(
-        './hidden/article-manager.html', 
+        './hidden/db-manager.html', 
         cover_picture=page_settings['cover_picture'], 
         current_page_num=page_settings['current_page'], 
         footer=page_settings['footer'],
@@ -425,6 +557,7 @@ def articleDatabaseViewer():
     dbsubscribers = Subscription.query.all()
     dbwriters = Writer.query.all()
     dbarticles = Article.query.all()
+    dbevents = Event.query.all()
 
     return render_template(
         './hidden/db-viewer.html', 
@@ -435,4 +568,6 @@ def articleDatabaseViewer():
         sidebar_segments=page_settings['sidebar_segments'],
         subscribers=dbsubscribers,
         writers=dbwriters,
-        articles=dbarticles)
+        articles=dbarticles,
+        events=dbevents
+        )
