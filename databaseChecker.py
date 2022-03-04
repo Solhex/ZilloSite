ce04e0b07c025bad784ac1287e90536606e8b25f
solfer <fvern.witherial01@tutanota.com>
2022-03-04 17:57:25 +0000

    Updated the temp files

--- databaseChecker.py
+++ databaseChecker.py
@@ -1,3 +1,10 @@
+# WARNING this file is a temp file not to be used within the final product
+# this is just used to check if the databases are in working order and 
+# the infromation within them.
+
+# This file is NOT needed for the program to work and SHOULD be deleted if
+# it is still present here
+
 import sqlite3
 
 conn = sqlite3.connect(r'website\databases\article_database.db')
@@ -10,3 +17,13 @@ conn = sqlite3.connect(r'website\databases\email_subscriptions.db')
 c = conn.cursor()
 
 print(c.execute('SELECT * FROM Subscription'),c.fetchall())
+
+conn = sqlite3.connect(r'website\databases\event_database.db')
+c = conn.cursor()
+
+print(c.execute('SELECT * FROM Event'),c.fetchall())
+
+conn = sqlite3.connect(r'website\databases\volunteer_database.db')
+c = conn.cursor()
+
+print(c.execute('SELECT * FROM Volunteer'),c.fetchall())
