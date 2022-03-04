28d3ffbeacb96db10541aa6a27f2c2a56e480063
solfer <fvern.witherial01@tutanota.com>
2022-03-04 17:59:10 +0000

    Commented the code and replaced some content
    Added comments to the code
    Modified the footer to use divs,
    Changed style width:100%; to class w-100,

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from website import create_app  # imports create_app from the folder website
 app = create_app()              # initilizes the create_app
 
 if __name__ == '__main__':      # runs the code if this specific file is executed
-    app.run(debug=True)         # runs the website in debug mode
+    app.run(debug=True)         # runs the website in debug mode, Note: remove debug mode or set it to False when running the site online
