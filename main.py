60f945554650c075ed0f32a13420f8a7f6e5e1d5
solfer <fvern.witherial01@tutanota.com>
2022-03-07 17:57:58 +0000

    Updated a comment

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from website import create_app  # imports create_app from the folder website
 app = create_app()              # initilizes the create_app
 
 if __name__ == '__main__':      # runs the code if this specific file is executed
-    app.run(debug=True)         # runs the website in debug mode, Note: remove debug mode or set it to False when running the site online
+    app.run(debug=True)         # runs the website in debug mode, DO NOT ENABLE debug mode POST PRODUCTION as it can allow python code execution with the debugger pin
